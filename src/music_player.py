eee0d55785b234b0e367155f8c876a8f57ac7985
ZELO_EX <ljy180418995@outlook.com>
2026-08-12 23:03:29 +0800

    code(gui_display): write framework

--- src/music_player.py
+++ src/music_player.py
@@ -1,24 +1,20 @@
 import json
 import os
 import random
+from typing import Tuple
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 class MusicPlayer:
-    __playlist_index = []
-    __playlist_ptr = 0
-    # range: 0~len(self.__playlist_config)
-    __playlist_play_status = 0
-    # 0 Pause
-    # 1 Play
-    # -1 error(set error code and send)
-    __playlist_config = []
+    _playlist_index = []
+    _playlist_ptr = 0
+    # range: 0~len(self._playlist_config)
+    _playlist_config = []
 
     def __init__(self, file_path: str):
         MusicPlayer.load_config(self, file_path)
-        self.__playlist_index = [i for i in range(len(self.__playlist_config))]
-        random.shuffle(self.__playlist_index)
+        self._playlist_index = [i for i in range(len(self._playlist_config))]
+        random.shuffle(self._playlist_index)
 
         print("Init player: ")
         now_playlist = MusicPlayer.now_playlist(self)
@@ -28,43 +24,40 @@ class MusicPlayer:
         return
 
     def load_config(self, file_path: str):
-        file = open(file=file_path, mode="r")
-        file_data = file.read()
-        file.close()
+        try:
+            with open(file_path, mode="r", encoding="utf-8") as file:
+                self._playlist_config = json.load(file)
+        except FileNotFoundError:
+            print(f"Error: file not found -> {file_path}")
+        except json.JSONDecodeError as e:
+            print(f"Error: invaild JSON -> {e}")
 
-        self.__playlist_config = json.loads(file_data)
-        return
+    def now_song(self) -> Tuple[str, int]:
+        now_index = self._playlist_index[self._playlist_ptr]
+        return self._playlist_config[now_index]["origin"], now_index
 
-    def now_song(self) -> (str, int):
-        now_index = self.__playlist_index[self.__playlist_ptr]
-        return (self.__playlist_config[now_index]["origin"], now_index)
-
-    def now_playlist(self) -> (int, int):
-        return (self.__playlist_ptr, len(self.__playlist_config))
+    def now_playlist(self) -> Tuple[int, int]:
+        return (self._playlist_ptr, len(self._playlist_config))
 
     def move(self, offset: int):
-        self.__playlist_ptr += offset
-        self.__playlist_ptr %= len(self.__playlist_config)
+        self._playlist_ptr += offset
+        self._playlist_ptr %= len(self._playlist_config)
         now_playlist = MusicPlayer.now_playlist(self)
         now_song = MusicPlayer.now_song(self)
         print(f"now play [{now_playlist[0]}, {now_song[1]}]: {now_song[0]}")
         return
 
-    def play():
-        print("playing.")
-        return
-
-    def pause(self):
-        print("stopped.")
-        return
-
-
 def main():
     load_dotenv()
-    music_path = Path(os.getenv("CONFIG_PATH"))
-    music_file = os.getenv("CONFIG_FILE")
-    path = music_path / music_file
-    mp = MusicPlayer(path.absolute())
+    music_path = os.getenv("MUSIC_PATH")
+    music_file = os.getenv("MUSIC_FILE")
+    if music_path is None or music_file is None:
+        print("Env error: lose MUSIC_PATH or MUSIC_FILE")
+    assert music_path is not None
+    assert music_file is not None
+    path = Path(music_path) / music_file
+    abs_path = path.absolute()
+    mp = MusicPlayer(str(abs_path))
     mp.move(-1)
 
 
